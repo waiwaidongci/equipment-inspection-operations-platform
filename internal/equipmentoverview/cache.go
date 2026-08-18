0544b7e74b4376f9dac9e21bd047535e4d067e8c
waiwaidongci <786958579@qq.com>
2026-08-18 17:43:44 +0800

    fix: inspection-overview-snapshot-race-003

--- internal/equipmentoverview/cache.go
+++ internal/equipmentoverview/cache.go
@@ -2,10 +2,18 @@ package equipmentoverview
 
 type Cache struct{ states map[string]DeviceState }
 
+func copyStates(states map[string]DeviceState) map[string]DeviceState {
+	copied := make(map[string]DeviceState, len(states))
+	for code, state := range states {
+		copied[code] = state
+	}
+	return copied
+}
+
 func (cache *Cache) Save(states map[string]DeviceState) {
-	cache.states = states
+	cache.states = copyStates(states)
 }
 
 func (cache *Cache) Load() map[string]DeviceState {
-	return cache.states
+	return copyStates(cache.states)
 }
--- internal/equipmentoverview/store.go
+++ internal/equipmentoverview/store.go
@@ -21,7 +21,13 @@ func NewStore(states []DeviceState) *Store {
 }
 
 func (store *Store) Snapshot() map[string]DeviceState {
-	return store.devices
+	store.mu.RLock()
+	defer store.mu.RUnlock()
+	snapshot := make(map[string]DeviceState, len(store.devices))
+	for code, state := range store.devices {
+		snapshot[code] = state
+	}
+	return snapshot
 }
 
 func (store *Store) Upsert(state DeviceState) {
